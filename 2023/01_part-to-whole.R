fe6f87204b6bda2ad270a10440e0bc7e945b30ed
Martijn van Bloois <mvbloois@gmail.com>
2023-03-08 20:09:42 +0100

    Part-to-whole

--- 2023/01_part-to-whole.R
+++ 2023/01_part-to-whole.R
@@ -5,12 +5,12 @@ library(showtext)
 source("./2023/00_functions.R")
 
 ## Text and colours ----
-txt <- "darkgreen"
+txt <- "#CA132F"
 bg  <- "#C5C5C5"
 
-source <- "Whatever by Whomever"
+source <- "eerstekamer\\.nl"
 
-title <- "PART-TO-WHOLE"
+title <- "EERSTE KAMER DER STATEN-GENERAAL"
 
 subtitle <- paste0(
   "Sentiment analysis based on 111.000+ tweets in 14 African languages shows<br>",
@@ -19,6 +19,12 @@ subtitle <- paste0(
   "<span style = 'color:#F7C608'>neutral</span> and ",
   "<span style = 'color:#952038'>negative</span> sentiments."
 )
+
+subtitle <- str_wrap("On May 30th, 2023 the members of the States-Provincial and \
+the electoral colleges of Bonaire, Saint Eustatius and Saba elect the new Senate \
+of the Dutch States General. In this chart we compare the election results of \
+2019 with the estimates based on the elections of the States-Provincial of March \
+15th, 2023.", 80)
 
 caption <- create_caption(txt, bg, source)
 
@@ -31,7 +37,7 @@ showtext_auto()
 theme_set(theme_void())
 
 theme_update(
-  text = element_text(family = "mainfont",
+  text = element_text(family = "titlefont",
                       size = 62, lineheight = 0.3),
   plot.background = element_rect(fill = bg,
                                  colour = bg),
@@ -39,14 +45,15 @@ theme_update(
   plot.caption.position = "plot",
   plot.title = element_text(family = "titlefont",
                             face = "bold",
+                            colour = txt,
                             margin = margin(t = 5, b = 5),
                             size = 102,
                             hjust = 0.5),
-  plot.subtitle = element_markdown(
+  plot.subtitle = element_text(family = "titlefont",
                                    size = 62,
                                    hjust = 0.5
                                    ),
-  plot.caption = element_markdown(family = "mainfont",
+  plot.caption = element_markdown(family = "titlefont",
                                   size = 62,
                                   colour = "grey15",
                                   hjust = 0.5,
@@ -55,25 +62,78 @@ theme_update(
 )
 
 ## Data ----
-df <- tibble::tibble(
-  var1 = c("A", "A", "A", "B", "B"),
-  var2 = c("X", "Y", "Z", "Y", "Z"),
-  var3 = c(33, 33, 34, 50, 50)
-)
+df <- tribble(
+  ~meting, ~fractie, ~zetels,
+  1, "VVD"    , 12,
+  1, "FvD"    , 12,
+  1, "CDA"   ,  9,
+  1, "GroenLinks", 8,
+  1, "D66"    ,  7,
+  1, "PvdA"   ,  6,
+  1, "PVV"    ,  5,
+  1, "SP"    ,  4,
+  1, "ChristenUnie", 4,
+  1, "PvdD"   ,  3,
+  1, "50PLUS" ,  2,
+  1, "SGP"    ,  2,
+  1, "OSF"    ,  1,
+  2, "VVD"    , 12,
+  2, "FvD"    , 1,
+  2, "Fractie-Nanninga", 7,
+  2, "Fractie-Otten", 2,
+  2, "Fractie-Frentrop", 2, 
+  2, "CDA"   ,  9,
+  2, "GroenLinks", 8,
+  2, "D66"    ,  7,
+  2, "PvdA"   ,  6,
+  2, "PVV"    ,  5,
+  2, "SP"    ,  4,
+  2, "ChristenUnie", 4,
+  2, "PvdD"   ,  3,
+  2, "50PLUS" ,  2,
+  2, "SGP"    ,  2,
+  2, "OSF"    ,  1,
+  
+  3, "VVD"    , 15,
+  3, "FvD"    , 2,
+  3, "BBB", 9,
+  3, "JA21", 3,
+  3, "Volt", 1, 
+  3, "CDA"   ,  5,
+  3, "GroenLinks", 8,
+  3, "D66"    ,  6,
+  3, "PvdA"   ,  5,
+  3, "PVV"    ,  9,
+  3, "SP"    ,  5,
+  3, "ChristenUnie", 2,
+  3, "PvdD"   ,  4,
+  3, "SGP"    ,  1
+) 
+
+spectrum <- 
+  c("SP", "PvdD", "50PLUS", "GroenLinks", "PvdA", "Volt", "D66",
+    "ChristenUnie", "CDA", "VVD", "BBB", "JA21", "PVV", "FvD", "SGP", "OSF")
 
 ## Plot ----
 
-df %>% 
-  mutate(var1 = as.factor(var1),
-         var2 = as.factor(var2)) %>% 
-  ggplot(aes(x = as.numeric(var1), y = var3, fill = var2)) +
-  geom_col(linewidth = 1, position = position_stack()) +
-  xlim(c(-1,3)) +
+df  %>% 
+  filter(meting != 2) %>% 
+  mutate(fractie = factor(fractie, levels = spectrum)) %>% 
+  ggplot(aes(x = meting, y = zetels, fill = fractie)) +
+  geom_col(colour = "white", linewidth = 1, position = position_stack()) +
+  geom_text(aes(x = 1, y = -4, label = "2019"),
+            family = "titlefont",
+            size = 25) +
+  geom_text(aes(x = 3, y = -4, label = "2023"),
+            family = "titlefont",
+            size = 30) +
+  xlim(c(-1,4)) +
+  ylim(c(-4, 79)) +
   coord_polar(theta = "y") +
   labs(title = title,
        subtitle = subtitle,
        caption = caption,
-       fill = "Colours") 
+       fill = "Fractie") 
 
 ## Saving ----
 ggsave("./2023/01_part-to-whole.png",
