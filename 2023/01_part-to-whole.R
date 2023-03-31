b71f41eefd82c45be3f5f4cfedbc7d4b0e351c92
Martijn van Bloois <mvbloois@gmail.com>
2023-03-31 15:48:42 +0200

    Final version

--- 2023/01_part-to-whole.R
+++ 2023/01_part-to-whole.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(ggtext)
 library(showtext)
+library(RColorBrewer)
 source("./2023/00_functions.R")
 
 ## Text and colours ----
@@ -12,18 +13,10 @@ source <- "eerstekamer\\.nl"
 
 title <- "EERSTE KAMER DER STATEN-GENERAAL"
 
-subtitle <- paste0(
-  "Sentiment analysis based on 111.000+ tweets in 14 African languages shows<br>",
-  "some interesting differences in ",
-  "<span style = 'color:#319400'>positive</span>, ",
-  "<span style = 'color:#F7C608'>neutral</span> and ",
-  "<span style = 'color:#952038'>negative</span> sentiments."
-)
-
 subtitle <- str_wrap("On May 30th, 2023 the members of the States-Provincial and \
 the electoral colleges of Bonaire, Saint Eustatius and Saba elect the new Senate \
 of the Dutch States General. In this chart we compare the election results of \
-2019 with the estimates based on the elections of the States-Provincial of March \
+2019 with the estimates based on the elections for the States-Provincial of March \
 15th, 2023.", 80)
 
 caption <- create_caption(txt, bg, source)
@@ -77,37 +70,23 @@ df <- tribble(
   1, "50PLUS" ,  2,
   1, "SGP"    ,  2,
   1, "OSF"    ,  1,
-  2, "VVD"    , 12,
-  2, "FvD"    , 1,
-  2, "Fractie-Nanninga", 7,
-  2, "Fractie-Otten", 2,
-  2, "Fractie-Frentrop", 2, 
-  2, "CDA"   ,  9,
-  2, "GroenLinks", 8,
-  2, "D66"    ,  7,
-  2, "PvdA"   ,  6,
-  2, "PVV"    ,  5,
-  2, "SP"    ,  4,
-  2, "ChristenUnie", 4,
-  2, "PvdD"   ,  3,
-  2, "50PLUS" ,  2,
-  2, "SGP"    ,  2,
-  2, "OSF"    ,  1,
-  
-  3, "VVD"    , 15,
+  3, "VVD"    , 10,
   3, "FvD"    , 2,
-  3, "BBB", 9,
+  3, "BBB", 17,
   3, "JA21", 3,
   3, "Volt", 1, 
   3, "CDA"   ,  5,
   3, "GroenLinks", 8,
-  3, "D66"    ,  6,
-  3, "PvdA"   ,  5,
-  3, "PVV"    ,  9,
-  3, "SP"    ,  5,
+  3, "D66"    ,  5,
+  3, "PvdA"   ,  7,
+  3, "PVV"    ,  5,
+  3, "SP"    ,  3,
   3, "ChristenUnie", 2,
   3, "PvdD"   ,  4,
-  3, "SGP"    ,  1
+  3, "SGP"    ,  1,
+  3, "OSF", 1,
+  3, "50PLUS", 1
+  
 ) 
 
 spectrum <- 
@@ -116,11 +95,14 @@ spectrum <-
 
 ## Plot ----
 
+getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+
 df  %>% 
   filter(meting != 2) %>% 
   mutate(fractie = factor(fractie, levels = spectrum)) %>% 
   ggplot(aes(x = meting, y = zetels, fill = fractie)) +
   geom_col(colour = "white", linewidth = 1, position = position_stack()) +
+  scale_fill_manual(values = getPalette(16)) +
   geom_text(aes(x = 1, y = -4, label = "2019"),
             family = "titlefont",
             size = 25) +
@@ -133,7 +115,7 @@ df  %>%
   labs(title = title,
        subtitle = subtitle,
        caption = caption,
-       fill = "Fractie") 
+       fill = "Party") 
 
 ## Saving ----
 ggsave("./2023/01_part-to-whole.png",
