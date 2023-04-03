06137687331029ce09e16b29064fc362b6b3d866
Martijn van Bloois <mvbloois@gmail.com>
2023-04-03 15:25:37 +0200

    Final version day 4

--- 2023/04_historical.R
+++ 2023/04_historical.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggtext)
 library(ggpattern)
 library(showtext)
 
@@ -30,12 +31,12 @@ theme_update(
                             size = 62,
                             hjust = 0.5),
   plot.subtitle = element_text(family = "font",
-                               size = 55,
+                               size = 52,
                                hjust = 0.5,
                                margin = margin(t = 5, b = 5)
   ),
   plot.caption = element_markdown(family = "font",
-                                  size = 32,
+                                  size = 52,
                                   colour = txt,
                                   hjust = 1,
                                   margin = margin(t = 15)),
@@ -46,15 +47,23 @@ theme_update(
 )
 
 df %>% 
-ggplot(aes(x = year, y = price)) +
-  geom_col_pattern(pattern_spacing = 0.01) +
+  ggplot(aes(x = year, y = price)) +
+  geom_col_pattern(
+    fill            = 'gray30',
+    colour          = 'gray20', 
+    pattern_density = 0.01, 
+    pattern_fill    = 'gray20',
+    pattern_colour  = 'gray20'
+  ) +
   scale_y_continuous(breaks = 1:4*240,
                      labels = c("£1", "£2", "£3", "£4")) +
+  #scale_pattern_spacing_discrete(range = c(0.01, 0.05)) +
   labs(title = str_wrap("PRICES OF THE QUARTER OF NINE BUSHELS OF THE BEST OR HIGHEST PRICED WHEAT AT WINDSOR MARKET, ON LADY-DAY AND MICHAELMAS, FROM 1595 TO 1764, BOTH INCLUSIVE; THE PRICE OF EACH YEAR BEING THE MEDIUM BETWEEN THE HIGHEST PRICES OF THOSE TWO MARKET-DAYS.", 60),
-       caption = "Data: Smith, Adam (1776), An Inquiry into the Nature and Causes of the Wealth of Nations<br>
-       Correspondence: Mr. M. van Bloois, Gouda, HOL-LAND",
+       caption = "Writings by the honourable Mr. A. Smith: An Inquiry into the Nature and Causes of the Wealth of Nations (1776)<br>
+       Drawing by Mr. M. van Bloois, city of Gouda, province of Holland",
        x = NULL,
        y = NULL) 
+
 ## Saving ----
 ggsave("./2023/04_historical.png",
        bg = bg,
