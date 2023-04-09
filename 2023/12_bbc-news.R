cd15b40ecb4cce3260c5a7c937caa81db233c299
Martijn van Bloois <martijn@Martijns-MacBook-Air.local>
2023-04-09 11:01:16 +0200

    Final version day 12

--- 2023/12_bbc-news.R
+++ 2023/12_bbc-news.R
@@ -17,31 +17,44 @@ df <- read_delim("./2023/data/tour_dem_tttot_tabular.tsv.gz") %>%
                names_prefix = "x", 
                values_to = "trips") 
 
-df %>% 
+plot_data <- df %>% 
   filter(geo == "NL") %>% 
   filter(c_dest != "WORLD") %>% 
   filter(purpose == "PER") %>%
   filter(duration == "N_GE4") %>% 
   filter(year >= "2016") %>% 
   mutate(c_dest = case_when(c_dest == "DOM" ~ "Domestic",
-                            c_dest == "FOR" ~ "Abroad",
-                            TRUE ~ "xx")) %>% 
+                            c_dest == "FOR" ~ "International",
+                            TRUE ~ "xx"))
+
+plot_data %>% 
   ggplot(aes(x = year, y =trips, group = c_dest)) +
-  geom_line(aes(colour = c_dest), size = 1) +
+  geom_line(aes(colour = c_dest), linewidth = 1.2) +
   geom_hline(yintercept = 0, size = 1, colour="#333333") +
+  geom_text(aes(x = "2020", y = 3.4e6,
+                 label = "In 2020 travel restrictions caused a fall\nof the number of trips abroad of 65%"),
+             size = 7.5,
+             colour = "#FAAB18"
+             ) +
+  geom_text(aes(x = "2021", y = 11e6,
+                label = "Domestic trips\nare up by 26%"),
+            size = 7.5,
+            colour = "#1380A1"
+  ) +
   scale_colour_manual(values = c("#1380A1", "#FAAB18")) +
   scale_y_continuous(limits = c(0,NA), 
                      breaks = c(5e6, 10e6, 15e6),
                      labels = number_format(scale = 1/1e6,
                                             suffix = "m")) +
   bbc_style() +
-  labs(title = "Dutch holidays",
-       subtitle = "Personal trips over 3 nights 2016-2021",
+  labs(title = "Going Dutch in times of covid-19",
+       subtitle = "Personal domestic and international trips over 3 nights 2016-2021",
        caption = "Source: Eurostat") +
   theme(axis.text.y = element_text(vjust = -0.1),
         panel.grid.major.y = element_line(color="#cbcbcb"), 
         panel.grid.major.x =element_blank(),
-        plot.caption = element_text(hjust = 0)
+        plot.caption = element_text(size = 14,
+                                    hjust = 0)
         )
 
 ## Saving ----
