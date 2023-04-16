5f13899a31b8c5a2076612ce11eff674822be59c
Martijn van Bloois <mvbloois@gmail.com>
2023-04-16 21:12:58 +0200

    Day 17 networks

--- 2023/17_networks.R
+++ 2023/17_networks.R
@@ -1,86 +1,113 @@
 library(tidyverse)
+library(showtext)
 
-df <- readxl::read_xlsx("./2023/data/rail_tf_ns_nl_page_spreadsheet.xlsx",
-                  sheet = 3,
-                  skip = 9) %>% 
+## Fonts ----
+font_add_google("Merriweather", "font")
+showtext_auto()
+
+df <-
+  readxl::read_xlsx(
+    "./2023/data/rail_tf_ns20_nl__custom_5802345_page_spreadsheet.xlsx",
+    sheet = 3,
+    skip = 9
+  ) %>%
   rename(segment = 1,
-         passengers = 2) %>%
-  mutate(passengers = parse_number(passengers)) %>% 
-  drop_na() %>%  
-  filter(is.numeric(passengers)) %>% 
+         trains = 2) %>%
+  mutate(segment = str_to_lower(segment)) %>%
+  drop_na() %>%
+  filter(is.numeric(trains)) %>%
+  filter(!str_detect(segment, "hsl")) %>%
+  filter(!str_detect(segment, "betuweroute")) %>%
   separate(segment, into = c("to", "from"),
-           sep = "-") %>% 
+           sep = "-") %>%
   mutate(across(to:from, str_squish))
 
-stations <- tibble(
-  station = unique(df$to, df$from)
-) 
-  filter(!str_detect(station, "Aansluiting")) %>% 
-  head(10) %>% 
-  mutate(x = tmaptools::geocode_OSM(paste("station", station))$coords[1],
-         y = tmaptools::geocode_OSM(paste("station", station))$coords[2])
+stations <-
+  read_csv("./2023/data/stations_nl.csv") %>% mutate(station = str_to_lower(station))
 
-  
-dd <- map(stations, ~tmaptools::geocode_OSM(paste("station", .x)))
- 
-stats <- dd[[1]] %>% 
-  select(query, lat, lon) %>% 
-  bind_rows(
-    tribble(
-      ~query, ~lat, ~lon,
-      "Moordrecht Aansluiting", 52.0109, 4.6655,
-      "Utrecht Lunetten Aansluiting", 52.0722, 5.1365,
-      "Harmelen Aansluiting", 52.1046, 4.9586,
-      "Amersfoort Schothorst", 52.1739, 5.4036,
-      "Kijfhoek", 51.8324, 4.5964,
-      "Venserpolder Aansluiting", 52.32667, 4.94728,
-      "Singelgracht Aansluiting", 52.3858, 4.8861,
-      "Weesp", 52.31265, 5.04362,
-      "Amsterdam Riekerpolder", 52.33844, 4.82615,
-      "Amsterdam Riekerpolder Aansluiting", 52.33844, 4.82615,
-      "Gaasperdammerweg Aansluiting", 52.3340, 4.9968,
-      "Hemtunnel Aansluiting", 52.4464, 4.8120,
-      "Vught Aansluiting", 51.6611, 5.2935,
-      "Hilversum", 52.22619, 5.18268,
-      "Geleen Lutterade Dsm Aansluiting", 50.97587, 5.82530,
-      "Onnen", 53.1572, 6.6340,
-      "Roosendaal Grens", 51.4695, 4.4524,
-      "S Heer Arendskerke Aansluiting", 51.49143, 3.83558,
-      "Waterhuizen Aansluiting", 53.1858, 6.6436,
-      "Eijsden Grens", 50.7591, 5.7064,
-      "Venlo Grens", 51.34095, 6.19452,
-   #   "Haanrade",
-      "Haanrade Grens", 50.88077, 6.08548,
-      "Zevenaar Grens",51.9012, 6.1411,
-   "Nieuweschans Grens", 53.1877, 7.2181,
-   "Oldenzaal Grens", 52.3091, 7.0440
+
+df_2 <- left_join(df,
+                  stations %>% select(station, from_lon = lon, from_lat = lat)
+                  ,
+                  by = join_by(to == station)) %>%
+  left_join(stations %>% select(station, to_lon = lon, to_lat = lat)
+            ,
+            by = join_by(from == station))
+
+brk <- c(5000, 30e3, 55e3, 75e3, 100e3, 125e3, 150e3)
+cols <-  rev(c('#0078FF', '#BD00FF', '#FF9A00', '#01FF1F', '#E3FF00'))
+newcol <- colorRampPalette(cols)
+ncols <- 7
+cols2 <- newcol(ncols)
+vmin <- min(df$values, na.rm = TRUE)
+vmax <- max(df$values, na.rm = TRUE)
+bg <- "#383E42"
+
+
+ggplot(stations, aes(x = lon, y = lat)) +
+  geom_segment(
+    data = df_2,
+    aes(
+      x = to_lon,
+      xend = from_lon,
+      y = to_lat,
+      yend = from_lat,
+      colour = trains,
+      size = trains
     )
-  ) 
-
-df_2 <- left_join(
-  df,
-  stats %>% 
-    mutate(station = str_remove(query, "station ")) %>% 
-    select(station, to_lat = lat, to_lon = lon)
-  , by = join_by(to == station)
-) %>% 
-  left_join(
-    stats %>% 
-      mutate(station = str_remove(query, "station ")) %>% 
-      select(station, from_lat = lat, from_lon = lon)
-    , by = join_by(from == station)
-  )
-
-ggplot(stats, aes(x = lon, y = lat)) +
-  geom_point(colour = "darkred",
-             size = 1.5) +
-  geom_segment(data = df_2,
-               aes(x = to_lon, xend = from_lon,
-                   y = to_lat, yend = from_lat,
-                   colour = passengers),
-               size = 1.2
-               ) +
-  scale_colour_gradient(low = "blue", high = "red") +
+  ) +
+  geom_point(
+    data = stations %>% filter(is_station == 1),
+    aes(x = lon, y = lat),
+    colour = "white",
+    size = 1.5
+  ) +
+  scale_colour_gradientn(
+    name = "# trains",
+    colours = cols2,
+    breaks = brk,
+    labels = scales::label_number(),
+    limits = c(min(brk), max(brk))
+  ) +
+  scale_size_continuous(range = c(0.2, 2.5)) +
   coord_equal() +
-  theme_minimal()
+  labs(title = "Train traffic in the Netherlands",
+       subtitle = "Number of passenger trains on part of the rail network (2020)",
+       caption = "Data: Eurostat") +
+  guides(size = "none",
+         colour = guide_legend(
+           direction = "horizontal",
+           keyheight = unit(0.5, units = "mm"),
+           keywidth = unit(14, units = "mm"),
+           title.position = 'left',
+           title.hjust = 0.5,
+           title.vjust = 1,
+           label.hjust = 0.5,
+           label.vjust = 5,
+           nrow = 1,
+           byrow = TRUE,
+           reverse = F,
+           label.position = "bottom"
+         )) +
+  theme_void() +
+  theme(text = element_text(family ="font",
+                            size = 35,
+                            colour = "white"),
+        plot.background = element_rect(fill = bg),
+        plot.title.position = "plot",
+        plot.title = element_text(size = 92,
+                                  hjust = 0.5),
+        plot.subtitle = element_text(size = 55,
+                                     hjust = 0.5),
+        plot.caption = element_text(size = 40,
+                                    hjust = 0.5),
+        legend.position = "bottom",
+        plot.margin = margin(b = 20, t = 20, r = 20, l = 20)
+        )
 
+## Saving ----
+ggsave("./2023/17_networks.png",
+       bg = bg,
+       height = 9, width = 9)
+
+system("open ./2023/17_networks.png")
