ca51831bf81f48682d3b5430974045a6bf74bbd4
Martijn van Bloois <mvbloois@gmail.com>
2024-04-06 08:02:24 +0200

    day 6 update

--- 2024/R/06_oecd.R
+++ 2024/R/06_oecd.R
@@ -23,9 +23,11 @@ hl <- c("Iceland", "Finland", "Sweden", "Denmark", "Norway", "Netherlands")
 plot_data |> 
   ggplot(aes(x = year_study, y = average,
              colour = jurisdiction)) +
-  geom_line(aes(group = jurisdiction)) +
+  geom_line(aes(group = jurisdiction),
+            linewidth = 0.8) +
   gghighlight(jurisdiction %in% hl,
-              use_direct_label = FALSE) +
+              use_direct_label = FALSE,
+              unhighlighted_params = list(linewidth = 0.2)) +
   facet_wrap(~jurisdiction) +
   scale_y_continuous(limits = c(0, 600),
                      breaks = c(0,100,200,300,400,500,600)) +
@@ -33,17 +35,30 @@ plot_data |>
   labs(x = NULL,
        y = NULL,
        title = "PISA Reading Scales",
-       subtitle = str_wrap("The Programme for International Student Assessment, conducted by the OECD, evaluates the abilities of 15-year-olds in reading, mathematics, and science to tackle real-world challenges. Several countries in Europe see a notable decline in reading skills.", 70),
+       subtitle = str_wrap("The Programme for International Student Assessment, conducted by the OECD, evaluates the abilities of 15-year-olds in reading, mathematics, and science to tackle real-world challenges. Several countries in Europe see a notable decline in reading skills.", 60),
        caption = "Data: OECD, PISA") +
   theme_minimal() +
   theme(
-    text = element_text(family = "font"),
+    text = element_text(family = "font",
+                        size = 24),
     legend.position = "none",
     panel.grid.minor = element_blank(),
-    strip.text = element_text(hjust = 0),
+    strip.text = element_text(size = 26,
+                              hjust = 0),
     plot.title.position = "plot",
     plot.title = element_text(family = "bold",
-                              size = 32),
+                              size = 62),
+    plot.subtitle = element_text(size = 42,
+                                 lineheight = 0.4),
+    plot.caption = element_text(size = 22),
     plot.margin = margin(10, 10, 10, 10)
   )
+
+ggsave(
+  "./2024/R/06_oecd.png",
+  bg = "white",
+  width = 8,
+  height = 6,
+)
+system("open ./2024/R/06_oecd.png")
 
