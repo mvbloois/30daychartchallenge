a7e944006243fa8dd6586b0a6f83e24f5f60ac0a
Martijn van Bloois <mvbloois@gmail.com>
2024-04-12 10:22:06 +0200

    Day 12

--- 2024/R/12_reuters-graphics.R
+++ 2024/R/12_reuters-graphics.R
@@ -29,23 +29,34 @@ plot_data <- df %>%
                             c_dest == "FOR" ~ "International",
                             TRUE ~ "xx"))
 
+labs_y <- tibble(
+  x = rep("2015", 4),
+  y = c(0 + 3e5, 5e6+3e5, 10e6+3e5, 15e6+3e5),
+  lbl = c("0", "5", "10", "15 milion")
+)
+
 plot_data %>% 
   ggplot(aes(x = year, y =trips, group = c_dest)) +
-  geom_line(aes(colour = c_dest), linewidth = 1.2) +
-  #geom_hline(yintercept = 0, linewidth = 0.8, colour="#333333") +
+  geom_line(aes(colour = c_dest), linewidth = 1.5) +
   scale_colour_manual(values = c("#308ecb", "#8b6fab")) +
   scale_x_discrete(breaks = c(2017, 2018, 2019, 2020, 2021, 2022)) +
   scale_y_continuous(limits = c(0,NA), 
                      breaks = c(0, 5e6, 10e6, 15e6),
                      labels = number_format(scale = 1/1e6,
                                             suffix = "m")) +
-
+  geom_text(data = labs_y,
+            aes(x = x, y = y, label = lbl),
+            hjust = 0,
+            size = 11,
+            colour = "grey30",
+            inherit.aes = FALSE) +
   labs(x = NULL, y = NULL,
        title = "Dutch trips: domestic and abroad",
        caption = "Source: Eurostat") +
+  coord_cartesian(expand = FALSE) +
   theme(
     text = element_text(family = "font",
-                        size = 32),
+                        size = 36),
     plot.background = element_rect(fill = "white",
                                    colour = "white"),
     legend.position = "top",
@@ -56,18 +67,16 @@ plot_data %>%
                                     colour = "white"),
     panel.grid.major.y = element_line(color="#cbcbcb"), 
     panel.grid.major.x = element_blank(),
-    #axis.text.x = element_text(vjust = 1.5),
-    axis.text.y = element_text(vjust = -1.5,
-                               hjust = 4),
-    #axis.ticks.length.x = unit(5, "pt"),
+    axis.text.y = element_blank(),
     axis.ticks.x = element_line(linewidth = 0.3),
     axis.ticks.y = element_blank(),
+    axis.line.x = element_line(colour = "grey30"),
     plot.title = element_text(size = 62,
                               colour = "grey30"),
-    plot.caption = element_text(size = 28,
+    plot.caption = element_text(size = 32,
                                 colour = "grey30",
                                 hjust = 0),
-    plot.margin = margin(10, 40, 10, 0)
+    plot.margin = margin(20, 20, 20, 20)
   )
 
 ## Saving ----
@@ -76,3 +85,4 @@ ggsave("./2024/R/12_reuters-graphics.png",
        height = 6, width = 7)
 
 system("open ./2024/R/12_reuters-graphics.png")      
+
